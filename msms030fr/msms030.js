6d7b05f7d02350ec68a2402b81e48822302b1c7b
Renaud <renaud.wangler@ib.cegos.fr>
2024-05-14 12:47:04 +0200

    msms030 beta14

--- msms030fr/msms030.js
+++ msms030fr/msms030.js
@@ -6,7 +6,7 @@ else { godeployDomain = 'labxxxxxx' }
 
 //CRéation du div de saisie des domaines
 domainInput = document.createElement('div')
-domainInput.style.cssText = 'width:50%; background:white; border:solid 2px blue; position:fixed; top:25%; left:25%;'
+domainInput.style.cssText = 'width:50%; background:white; border:solid 2px blue; position:fixed; top:25%; right:0;'
 
 domainInputTitle = document.createElement('div')
 domainInputTitle.style.cssText = 'width:100%; height:25px; background: blue;color: white; font-weight:bold;'
@@ -21,7 +21,7 @@ domainInputTitle.appendChild(domainInputClose)
 
 domainInputContent = document.createElement('div')
 domainInputContent.style.cssText = 'padding: 5px'
-domainInputContent.innerHTML = 'Ici, vous pouvez saisir les noms de domaine de votre instance d\'atelier afin de vous en faciliter la réalisation.<br/><ul><li>Domaine original Microsoft 365 :<br/><input type="text" id="onmicrosoftDomain" value="' + onmicrosoftDomain + '">.onmicrosoft.com</li><li>Domaine de l\'atelier goDeploy :<br/>' + godeployDomain + '.godeploylabs.com</li></ul>'
+domainInputContent.innerHTML = 'Ici, vous pouvez saisir les noms de domaine de votre instance d\'atelier afin de vous en faciliter la réalisation.<br/><ul><li>Domaine original Microsoft 365 :<br/><input type="text" id="onmicrosoftDomain" value="' + onmicrosoftDomain + '">.onmicrosoft.com <a href="#"><img src="enterKey.png"></a></li><li>Domaine de l\'atelier goDeploy :<br/><input type="text" id="godeployDomain" value="' + godeployDomain + '">.godeploylabs.com <a href="#"><img src="enterKey.png"></a></li></ul>'
 
 domainInput.appendChild(domainInputContent)
 
