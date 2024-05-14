bce301142a89fbd63fd3aa9855eee24f2c764797
Renaud <renaud.wangler@ib.cegos.fr>
2024-05-14 13:31:36 +0200

    msms030.js beta20

--- msms030fr/msms030.js
+++ msms030fr/msms030.js
@@ -8,6 +8,11 @@ function updateDomains() {
     document.body.innerHTML = document.body.innerHTML.replace(/\[onmicrosoftDomain\]/g,onmicrosoftDomain)
     document.body.innerHTML = document.body.innerHTML.replace(/\[godeployDomain\]/g,godeployDomain) }
 
+function newDomains() {
+   console.log(document.getElementById('onmicrosoftDomainNew').value)
+   console.log(document.getElementById('godeployDomainNew').value)
+   location.reload()}
+
 //CRéation du div de saisie des domaines
 domainInput = document.createElement('div')
 domainInput.style.cssText = 'width:50%; background:white; border:solid 2px blue; position:fixed; top:25%; right:0;'
@@ -19,7 +24,7 @@ domainInput.appendChild(domainInputTitle)
 
 domainInputContent = document.createElement('div')
 domainInputContent.style.cssText = 'padding: 5px'
-domainInputContent.innerHTML = 'Ici, vous pouvez saisir les noms de domaine de votre instance d\'atelier afin de vous en faciliter la réalisation.<br/><ul><li>Domaine original Microsoft 365 :<br/><input type="text" id="onmicrosoftDomainNew" value="' + onmicrosoftDomain + '">.onmicrosoft.com <a href="#" onclick="console.log(document.getElementById(\'onmicrosoftDomainNew\').value)"><img src="enterKey.png"></a></li><br/><li>Domaine de l\'atelier goDeploy :<br/><input type="text" id="godeployDomainNew" value="' + godeployDomain + '">.godeploylabs.com <a href="#" onclick="console.log(document.getElementById(\'godeployDomainNew\').value)"><img src="enterKey.png"></a></li></ul>'
+domainInputContent.innerHTML = 'Ici, vous pouvez saisir les noms de domaine de votre instance d\'atelier afin de vous en faciliter la réalisation.<br/><ul><li>Domaine original Microsoft 365 :<br/><input type="text" id="onmicrosoftDomainNew" value="' + onmicrosoftDomain + '">.onmicrosoft.com</li><br/><li>Domaine de l\'atelier goDeploy :<br/><input type="text" id="godeployDomainNew" value="' + godeployDomain + '">.godeploylabs.com <a href="#" onclick="newDomains()"><img src="enterKey.png"></a></li></ul>'
 
 //localStorage.setItem('msms030-onmicrosoftDomain')
 
