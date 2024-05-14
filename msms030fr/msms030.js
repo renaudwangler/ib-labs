e494f4cad8f7c5fead1e13dba463e2cc1dcf11c4
Renaud <renaud.wangler@ib.cegos.fr>
2024-05-14 16:03:36 +0200

    msms030.js beta25

--- msms030fr/msms030.js
+++ msms030fr/msms030.js
@@ -5,7 +5,7 @@ function updateDomains() {
     if (localStorage.getItem('msms030-godeployDomain')) { godeployDomain = localStorage.getItem('msms030-godeployDomain') }
     else { godeployDomain = 'labxxxxxx' }
     //CRéation du div de saisie des domaines
-    if (!domainInput) {
+    if (typeof domainInput === 'undefined') {
         domainInput = document.createElement('div')
         domainInput.style.cssText = 'width:50%; background:white; border:solid 2px blue; position:fixed; top:25%; right:0; display:none;'
         domainInput.innerHTML = '<div style="width:100%; height:25px; background: blue;color: white; font-weight:bold">&nbsp;Noms des domaines <div style="width:21px; height:21px; line-height:21px; position:absolute; top:2px; right:2px; background:red; display:block; text-align:center; cursor: pointer;" onclick = "domainInput.style.display=\'none\'">X</div></div><div style="padding: 5px;">Ici, vous pouvez saisir les noms de domaine de votre instance d\'atelier afin de vous en faciliter la réalisation.<br/><ul><li>Domaine original Microsoft 365 :<br/>(peut être trouvé dans le nom de domaine de l\'administrateur "Office 365 Teannt Credentials" dans l\'onglet "Home" du volet de gauche de votre environnement goDeploy)<br/><input type="text" id="onmicrosoftDomainNew" value="' + onmicrosoftDomain + '">.onmicrosoft.com</li><br/><li>Domaine de l\'atelier goDeploy :<br/>(peut être trouvé sous le nom "Lab Domain" dans l\'onglet "DNS" du volet de gauche de votre environnement goDeploy)<br/><input type="text" id="godeployDomainNew" value="' + godeployDomain + '">.godeploylabs.com <a href="#" onclick="newDomains()"><img src="enterKey.png"></a><br/></li></ul></div>'}
