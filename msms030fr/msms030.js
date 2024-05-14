526bcd51c89c91a0a005da9feb1b509fa6aa151e
Renaud <renaud.wangler@ib.cegos.fr>
2024-05-14 10:57:34 +0200

    msms030 beta9

--- msms030fr/msms030.js
+++ msms030fr/msms030.js
@@ -6,22 +6,22 @@ else { godeployDomain = 'labxxxxxx' }
 
 //CRéation du div de saisie des domaines
 domainInput = document.createElement('div')
-domainInput.style.cssText = 'width:50%; height:50%; background:white; border:solid 2px blue; position:fixed; top:25%; left:25%'
-
+domainInput.style.cssText = 'width:50%; height:50%; background:white; border:solid 2px blue; position:fixed; top:25%; left:25%;'
 
 domainInputTitle = document.createElement('div')
-domainInputTitle.style.width = '100%'
-domainInputTitle.style.height = '25px'
-domainInputTitle.style.background = 'blue'
-domainInputTitle.style.color = 'white'
-domainInputTitle.style.fontWeight = 'bold'
+domainInputTitle.style.cssText = 'width:100%; height:25px; background: blue;color: white; font-weight:bold;'
 domainInputTitle.innerHTML = '&nbsp;Noms des domaines'
 domainInput.appendChild(domainInputTitle)
 
-domainInputContent = document.createElement('div')
-domainInputContent.style.padding = '5px'
-domainInputContent.innerHTML = 'Ici, vous pouvez saisir les noms de domaine de votre instance d\'atelier afin de vous en faciliter la réalisation.<br/><ul><li>Domaine original Microsoft 365 : ' + onmicrosoftDomain + '.onmicrosoft.com</li><li>Domaine de l\'atelier goDeploy : ' + godeployDomain + '.godeploylabs.com</li></ul>'
+domainInputClose = document.createElement('a')
+domainInputClose.style.cssText = 'width:21px; height:21px; position:absolute; top:2px; right:2px; background:red; display:block'
+domainInputClose.appendChild(document.createTextNode('x'))
+domainInputTitle.appendChild(domainInputClose)
 
+domainInputContent = document.createElement('div')
+domainInputContent.style.cssText = 'padding: 5px'
+domainInputContent.innerHTML = 'Ici, vous pouvez saisir les noms de domaine de votre instance d\'atelier afin de vous en faciliter la réalisation.<br/><ul><li>Domaine original Microsoft 365 : ' + onmicrosoftDomain + '.onmicrosoft.com</li><li>Domaine de l\'atelier goDeploy : ' + godeployDomain + '.godeploylabs.com</li></ul>'
 domainInput.appendChild(domainInputContent)
+
 
 document.addEventListener('DOMContentLoaded', function () { document.body.appendChild(domainInput) })
