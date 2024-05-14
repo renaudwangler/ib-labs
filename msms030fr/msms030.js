c01779e51e978e5a1f2842c4ecd61bd0fb4b2e61
Renaud <renaud.wangler@ib.cegos.fr>
2024-05-14 13:37:43 +0200

    msms030 beta22

--- msms030fr/msms030.js
+++ msms030fr/msms030.js
@@ -9,26 +9,13 @@ function updateDomains() {
     document.body.innerHTML = document.body.innerHTML.replace(/\[godeployDomain\]/g,godeployDomain) }
 
 function newDomains() {
-   console.log(document.getElementById('onmicrosoftDomainNew').value)
-   console.log(document.getElementById('godeployDomainNew').value)
+    localStorage.setItem('msms030-onmicrosoftDomain',document.getElementById('onmicrosoftDomainNew').value)
+    localStorage.setItem('msms030-godeployDomain',document.getElementById('godeployDomainNew').value)
    location.reload()}
 
 //CRéation du div de saisie des domaines
 domainInput = document.createElement('div')
 domainInput.style.cssText = 'width:50%; background:white; border:solid 2px blue; position:fixed; top:25%; right:0;'
-
-domainInputTitle = document.createElement('div')
-domainInputTitle.style.cssText = 'width:100%; height:25px; background: blue;color: white; font-weight:bold;'
-domainInputTitle.innerHTML = '&nbsp;Noms des domaines <div style="width:21px; height:21px; line-height:21px; position:absolute; top:2px; right:2px; background:red; display:block; text-align:center; cursor: pointer;" onclick = "domainInput.style.display=\'none\'">X</div>'
-domainInput.appendChild(domainInputTitle)
-
-domainInputContent = document.createElement('div')
-domainInputContent.style.cssText = 'padding: 5px'
-domainInputContent.innerHTML = 'Ici, vous pouvez saisir les noms de domaine de votre instance d\'atelier afin de vous en faciliter la réalisation.<br/><ul><li>Domaine original Microsoft 365 :<br/><input type="text" id="onmicrosoftDomainNew" value="' + onmicrosoftDomain + '">.onmicrosoft.com</li><br/><li>Domaine de l\'atelier goDeploy :<br/><input type="text" id="godeployDomainNew" value="' + godeployDomain + '">.godeploylabs.com <a href="#" onclick="newDomains()"><img src="enterKey.png"></a></li></ul>'
-
-//localStorage.setItem('msms030-onmicrosoftDomain')
-
-domainInput.appendChild(domainInputContent)
-
+domainInput.innerHTML = '<div style="width:100%; height:25px; background: blue;color: white; font-weight:bold">&nbsp;Noms des domaines <div style="width:21px; height:21px; line-height:21px; position:absolute; top:2px; right:2px; background:red; display:block; text-align:center; cursor: pointer;" onclick = "domainInput.style.display=\'none\'">X</div></div><div style="padding: 5px;">Ici, vous pouvez saisir les noms de domaine de votre instance d\'atelier afin de vous en faciliter la réalisation.<br/><ul><li>Domaine original Microsoft 365 :<br/><input type="text" id="onmicrosoftDomainNew" value="' + onmicrosoftDomain + '">.onmicrosoft.com</li><br/><li>Domaine de l\'atelier goDeploy :<br/><input type="text" id="godeployDomainNew" value="' + godeployDomain + '">.godeploylabs.com <a href="#" onclick="newDomains()"><img src="enterKey.png"></a></li></ul></div>'
 
 document.addEventListener('DOMContentLoaded', function () { updateDomains(); document.body.appendChild(domainInput) })
